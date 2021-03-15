fe696b28109cad037411d819e2a698c658914851
Elena Kanash <hird250@gmail.com>
2021-03-16 00:26:23 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n){
+    if (n < 0)
+    n = - n
+    let res = n.toString().split("").reverse().join("");
+    return res;
+  }
